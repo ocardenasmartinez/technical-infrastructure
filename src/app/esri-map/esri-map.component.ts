a7593ae572866b127d2abe8b1735bdcd1a9937c6
Octavio Ernesto Cárdenas Martínez <ocardenas@subtel.gob.cl>
2018-10-16 15:52:45 -0300

    add

--- src/app/esri-map/esri-map.component.ts
+++ src/app/esri-map/esri-map.component.ts
@@ -69,6 +69,131 @@ export class EsriMapComponent implements OnInit {
     }
   }
 
+  async setLayers() {
+
+    const [FeatureLayer] = await loadModules([
+      'esri/layers/FeatureLayer'
+    ]);
+
+    const nodosTelsur = new FeatureLayer({
+      url: urlConstants.NODOS_TELSUR,
+      title: 'Nodos Telesur 2'
+    });
+    const trazadoTelsurProject = new FeatureLayer({
+      url: urlConstants.TRAZADO_TELSUR_PROJECT,
+      title: 'Trazado Telesur Project'
+    });
+    const trazadoTelsur  = new FeatureLayer({
+      url: urlConstants.TRAZADO_TELSUR,
+      title: 'Trazado Telesur'
+    });
+    const borradorIC1MacrozonacentronorteWom  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_WOM,
+      title: 'Trazado Telesur'
+    });
+    const borradorIC1MacrozonacentronorteWill  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_WILL,
+      title: 'Macro Zona Centro Norte Will'
+    });
+    const borradorIC1MacrozonacentronorteVtRBandaAncha  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_VTR_BANDA_ANCHA,
+      title: 'Macro Zona Centro Norte VTR Banda Ancha'
+    });
+    const borradorIC1MacrozonacentronorteTelefonicaChile  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_TELEFONICA_CHILE,
+      title: 'Macro Zona Centro Norte Teléfonica Chile'
+    });
+    const borradorIC1MacrozonacentronorteMovistar  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_MOVISTAR,
+      title: 'Macro Zona Centro Norte Movistar'
+    });
+    const borradorIC1MacrozonacentronorteEntelPhone  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_ENTEL_PHONE,
+      title: 'Macro Zona Centro Norte Entel Phone'
+    });
+    const borradorIC1MacrozonacentronorteEntelPCS  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_ENTEL_PCS,
+      title: 'Macro Zona Centro Norte Entel PCS'
+    });
+    const borradorIC1MacrozonacentronorteClaroComunicaciones  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_CLARO_COMUNICACIONES,
+      title: 'Macro Zona Centro Norte Claro Comunicaciones'
+    });
+    const borradorIC1MacrozonacentronorteClaroChile  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_CLARO_CHILE,
+      title: 'Macro Zona Centro Norte Chile'
+    });
+    const borradorIC1MacrozonanorteWom  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_WOM,
+      title: 'Macro Zona Centro Norte Claro Chile'
+    });
+    const borradorIC1MacrozonanorteWill  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_WILL,
+      title: 'Macro Zona Norte Will'
+    });
+    const borradorIC1MacrozonanorteVtrBandaAncha  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_VTR_BANDA_ANCHA,
+      title: 'Macro Zona Norte VTR Banda Ancha'
+    });
+    const borradorIC1MacrozonanorteTelefonicaChile  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_TELEFONICA_CHILE,
+      title: 'Macro Zona Norte Teléfonica'
+    });
+    const borradorIC1MacrozonanorteMovistar  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_MOVISTAR,
+      title: 'Macro Zona Norte Movistar'
+    });
+    const borradorIC1MacrozonanorteEntelPhone  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_ENTEL_PHONE,
+      title: 'Macro Zona Norte Entel Phone'
+    });
+    const borradorIC1MacrozonanorteEntelPCS  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_ENTEL_PCS,
+      title: 'Macro Zona Norte Entel PCS'
+    });
+    const borradorIC1MacrozonanorteClaroComunicaciones  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_CLARO_COMUNICACIONES,
+      title: 'Macro Zona Norte Claro Comunicaciones'
+    });
+    const borradorIC1MacrozonanorteClaroChile  = new FeatureLayer({
+      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_CLARO_CHILE,
+      title: 'Macro Zona Norte Claro Chile'
+    });
+    const IC1_IC2_2016_2020  = new FeatureLayer({
+      url: urlConstants.IC1_IC2_2016_2020,
+      title: 'IC1 IC2 2016 2020'
+    });
+    const IC1_PLANFISCA_N5MAYO  = new FeatureLayer({
+      url: urlConstants.IC1_PLANFISCA_N5MAYO,
+      title: 'IC1 PLAN FISCA N5 MAYO'
+    });
+
+    this.mapView.map.add(IC1_IC2_2016_2020);
+    this.mapView.map.add(IC1_PLANFISCA_N5MAYO);
+    this.mapView.map.add(borradorIC1MacrozonanorteClaroChile);
+    this.mapView.map.add(borradorIC1MacrozonanorteClaroComunicaciones);
+    this.mapView.map.add(borradorIC1MacrozonanorteEntelPCS);
+    this.mapView.map.add(borradorIC1MacrozonanorteEntelPhone);
+    this.mapView.map.add(borradorIC1MacrozonanorteMovistar);
+    this.mapView.map.add(borradorIC1MacrozonanorteTelefonicaChile);
+    this.mapView.map.add(borradorIC1MacrozonanorteVtrBandaAncha);
+    this.mapView.map.add(borradorIC1MacrozonanorteWill);
+    this.mapView.map.add(borradorIC1MacrozonanorteWom);
+    this.mapView.map.add(borradorIC1MacrozonacentronorteClaroChile);
+    this.mapView.map.add(borradorIC1MacrozonacentronorteClaroComunicaciones);
+    this.mapView.map.add(borradorIC1MacrozonacentronorteEntelPCS);
+    this.mapView.map.add(borradorIC1MacrozonacentronorteEntelPhone);
+    this.mapView.map.add(borradorIC1MacrozonacentronorteMovistar);
+    this.mapView.map.add(borradorIC1MacrozonacentronorteTelefonicaChile);
+    this.mapView.map.add(borradorIC1MacrozonacentronorteVtRBandaAncha);
+    this.mapView.map.add(borradorIC1MacrozonacentronorteWill);
+    this.mapView.map.add(borradorIC1MacrozonacentronorteWom);
+    this.mapView.map.add(trazadoTelsur);
+    this.mapView.map.add(trazadoTelsurProject);
+    this.mapView.map.add(nodosTelsur);
+
+  }
+
   async setSearcher() {
 
     const [Search, FeatureLayer] = await loadModules([
@@ -404,132 +529,6 @@ export class EsriMapComponent implements OnInit {
     });
 
     this.mapView.ui.add(searchWidget, 'top-right');
-  }
-
-  async setLayers() {
-
-    const [FeatureLayer] = await loadModules([
-      'esri/layers/FeatureLayer'
-    ]);
-
-    const nodosTelsur = new FeatureLayer({
-      url: urlConstants.NODOS_TELSUR,
-      title: 'Nodos Telesur'
-    });
-    const trazadoTelsurProject = new FeatureLayer({
-      url: urlConstants.TRAZADO_TELSUR_PROJECT,
-      title: 'Trazado Telesur Project'
-    });
-    const trazadoTelsur  = new FeatureLayer({
-      url: urlConstants.TRAZADO_TELSUR,
-      title: 'Trazado Telesur'
-    });
-    const borradorIC1MacrozonacentronorteWom  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_WOM,
-      title: 'Trazado Telesur'
-    });
-    const borradorIC1MacrozonacentronorteWill  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_WILL,
-      title: 'Macro Zona Centro Norte Will'
-    });
-    const borradorIC1MacrozonacentronorteVtRBandaAncha  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_VTR_BANDA_ANCHA,
-      title: 'Macro Zona Centro Norte VTR Banda Ancha'
-    });
-    const borradorIC1MacrozonacentronorteTelefonicaChile  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_TELEFONICA_CHILE,
-      title: 'Macro Zona Centro Norte Teléfonica Chile'
-    });
-    const borradorIC1MacrozonacentronorteMovistar  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_MOVISTAR,
-      title: 'Macro Zona Centro Norte Movistar'
-    });
-    const borradorIC1MacrozonacentronorteEntelPhone  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_ENTEL_PHONE,
-      title: 'Macro Zona Centro Norte Entel Phone'
-    });
-    const borradorIC1MacrozonacentronorteEntelPCS  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_ENTEL_PCS,
-      title: 'Macro Zona Centro Norte Entel PCS'
-    });
-    const borradorIC1MacrozonacentronorteClaroComunicaciones  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_CLARO_COMUNICACIONES,
-      title: 'Macro Zona Centro Norte Claro Comunicaciones'
-
-    });
-    const borradorIC1MacrozonacentronorteClaroChile  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_CENTRO_NORTE_CLARO_CHILE,
-      title: 'Macro Zona Centro Norte Chile'
-    });
-    const borradorIC1MacrozonanorteWom  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_WOM,
-      title: 'Macro Zona Centro Norte Claro Chile'
-    });
-    const borradorIC1MacrozonanorteWill  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_WILL,
-      title: 'Macro Zona Norte Will'
-    });
-    const borradorIC1MacrozonanorteVtrBandaAncha  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_VTR_BANDA_ANCHA,
-      title: 'Macro Zona Norte VTR Banda Ancha'
-    });
-    const borradorIC1MacrozonanorteTelefonicaChile  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_TELEFONICA_CHILE,
-      title: 'Macro Zona Norte Teléfonica'
-    });
-    const borradorIC1MacrozonanorteMovistar  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_MOVISTAR,
-      title: 'Macro Zona Norte Movistar'
-    });
-    const borradorIC1MacrozonanorteEntelPhone  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_ENTEL_PHONE,
-      title: 'Macro Zona Norte Entel Phone'
-    });
-    const borradorIC1MacrozonanorteEntelPCS  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_ENTEL_PCS,
-      title: 'Macro Zona Norte Entel PCS'
-    });
-    const borradorIC1MacrozonanorteClaroComunicaciones  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_CLARO_COMUNICACIONES,
-      title: 'Macro Zona Norte Claro Comunicaciones'
-    });
-    const borradorIC1MacrozonanorteClaroChile  = new FeatureLayer({
-      url: urlConstants.BORRADOR_IC1MACRO_ZONA_NORTE_CLARO_CHILE,
-      title: 'Macro Zona Norte Claro Chile'
-    });
-    const IC1_IC2_2016_2020  = new FeatureLayer({
-      url: urlConstants.IC1_IC2_2016_2020,
-      title: 'IC1 IC2 2016 2020'
-    });
-    const IC1_PLANFISCA_N5MAYO  = new FeatureLayer({
-      url: urlConstants.IC1_PLANFISCA_N5MAYO,
-      title: 'IC1 PLAN FISCA N5 MAYO'
-    });
-
-    this.mapView.map.add(IC1_IC2_2016_2020);
-    this.mapView.map.add(IC1_PLANFISCA_N5MAYO);
-    this.mapView.map.add(borradorIC1MacrozonanorteClaroChile);
-    this.mapView.map.add(borradorIC1MacrozonanorteClaroComunicaciones);
-    this.mapView.map.add(borradorIC1MacrozonanorteEntelPCS);
-    this.mapView.map.add(borradorIC1MacrozonanorteEntelPhone);
-    this.mapView.map.add(borradorIC1MacrozonanorteMovistar);
-    this.mapView.map.add(borradorIC1MacrozonanorteTelefonicaChile);
-    this.mapView.map.add(borradorIC1MacrozonanorteVtrBandaAncha);
-    this.mapView.map.add(borradorIC1MacrozonanorteWill);
-    this.mapView.map.add(borradorIC1MacrozonanorteWom);
-    this.mapView.map.add(borradorIC1MacrozonacentronorteClaroChile);
-    this.mapView.map.add(borradorIC1MacrozonacentronorteClaroComunicaciones);
-    this.mapView.map.add(borradorIC1MacrozonacentronorteEntelPCS);
-    this.mapView.map.add(borradorIC1MacrozonacentronorteEntelPhone);
-    this.mapView.map.add(borradorIC1MacrozonacentronorteMovistar);
-    this.mapView.map.add(borradorIC1MacrozonacentronorteTelefonicaChile);
-    this.mapView.map.add(borradorIC1MacrozonacentronorteVtRBandaAncha);
-    this.mapView.map.add(borradorIC1MacrozonacentronorteWill);
-    this.mapView.map.add(borradorIC1MacrozonacentronorteWom);
-    this.mapView.map.add(trazadoTelsur);
-    this.mapView.map.add(trazadoTelsurProject);
-    this.mapView.map.add(nodosTelsur);
-
   }
 
 }
